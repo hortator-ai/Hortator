1d1b382ec7b41e7fc7d73bc902efb85dab30665d
Daemon <daemon@hortator.ai>
2026-02-17 10:01:45 +0100

    fix: sort imports for goimports linter

--- internal/controller/pod_builder.go
+++ internal/controller/pod_builder.go
@@ -17,9 +17,9 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
-	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	corev1alpha1 "github.com/hortator-ai/Hortator/api/v1alpha1"
